abd670aa4707343094d861b04eda1488031b1096
divko <test555_05@list.ru>
2024-02-09 11:08:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello");
+            Console.Write(" ,World");
+            Console.Write(" !!!");
+            Console.ReadKey();
+        }
+    }
+}
